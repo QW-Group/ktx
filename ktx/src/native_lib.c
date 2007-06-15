ccbef711ee357ef392dec2c8886e181e76c819a9
Ivan Bolsunov <ivan.bolsunov@gmail.com>
2007-06-15 18:37:28 +0000

    nothing, just removing bg_lib.c from native builds.

---
+++ ktx/src/native_lib.c
@@ -0,0 +1,48 @@
+/*
+ * $Id: native_lib.c,v 1.1 2007/06/15 18:37:28 qqshka Exp $
+ */
+
+// native_lib.c -- different OS'es have different lib functions implementation, work around.
+
+// this file is only included when building a dll
+#ifdef Q3_VM
+#error "Do not use in VM build"
+#endif
+
+#include "g_local.h"
+
+
+#ifdef _WIN32
+
+// ripped from VVD code
+
+int Q_vsnprintf(char *buffer, size_t count, const char *format, va_list argptr)
+{
+	int ret;
+	if (!count)
+		return 0;
+
+	ret = _vsnprintf(buffer, count, format, argptr);
+	buffer[count - 1] = 0;
+
+	return ret;
+}
+
+int snprintf(char *buffer, size_t count, char const *format, ...)
+{
+	int ret;
+	va_list argptr;
+
+	if (!count)
+		return 0;
+
+	va_start(argptr, format);
+	ret = _vsnprintf(buffer, count, format, argptr);
+	buffer[count - 1] = 0;
+	va_end(argptr);
+
+	return ret;
+}
+
+#endif // _WIN32
+
