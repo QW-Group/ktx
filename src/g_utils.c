868324f8021f962581db0166a137aa0d74d2a9cb
meag <meag@acm.org>
2016-01-08 12:50:59 +0000

    ENTITY FILES: Support multiple .ent files for each map.
    Any .ent files in the maps or entfiles directories which are in format mapName#xxx.ent
    will be added to the maps list.  If selected, sv_entityfile will be set to the name of
    the entity file, and the map changed to mapName.
    
    Requires server support for sv_entityfile cvar.

--- src/g_utils.c
+++ src/g_utils.c
@@ -1399,13 +1399,28 @@ qbool SetHandicap( gedict_t *p, int nhdc )
 
 void changelevel( const char *name )
 {
+	char* entityFileSep = NULL;
+
  	if ( strnull( name ) )
 		G_Error("changelevel: null");
 
 	if ( isRACE() && race.race_recording )
 		race_stoprecord( true );
 
-	trap_changelevel(name);
+	entityFileSep = strchr(name, SV_ENTITYFILE_SEPARATOR);
+	if (entityFileSep)
+	{
+		char mapName[128] = { 0 };
+
+		cvar_set("sv_entityfile", name);
+		strlcpy(mapName, (char*)name, min(entityFileSep - name + 1, sizeof(mapName) / sizeof(mapName[0])));
+		trap_changelevel(mapName);
+	}
+	else
+	{
+		cvar_set("sv_entityfile", "");
+		trap_changelevel(name);
+	}
 }
 
 char *Get_PowerupsStr(void)
