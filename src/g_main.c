fff6f4182756c0f223b7c55f536859ef61bcdaf1
Alexandre Nizoux <alexandre@nizoux.fr>
2020-04-22 10:09:04 +0200

    SERVERINFO: Removed newly added keys, forgot how length limited it is

--- src/g_main.c
+++ src/g_main.c
@@ -443,9 +443,9 @@ void G_InitGame( int levelTime, int randomSeed )
 
 	// put mod version in serverinfo
 	localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_MOD_KEY, MOD_VERSION );
-	localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_BUILD_KEY, GIT_COMMIT );
-	localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_DATE_KEY, MOD_BUILD_DATE );
-	localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_URL_KEY, MOD_URL );
+	//localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_BUILD_KEY, GIT_COMMIT );
+	//localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_DATE_KEY, MOD_BUILD_DATE );
+	//localcmd( "serverinfo \"%s\" \"%s\"\n", MOD_SERVERINFO_URL_KEY, MOD_URL );
 }
 
 void G_ShutDown()
