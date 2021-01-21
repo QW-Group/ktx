5cf22c0888723a47f3b5199924a957279dc17d89
meag <meag@users.noreply.github.com>
2021-01-21 16:30:23 +0000

    PREWAR: Option for non-random pellet distribution in prewar
    nrb=1 will give yawnmode bullet distribution, useful to see
      the maximum range of shotgun

--- src/weapons.c
+++ src/weapons.c
@@ -511,6 +511,7 @@ void FireBullets( float shotcount, vec3_t dir, float spread_x, float spread_y, f
 	vec3_t          direction;
 	vec3_t          src, tmp, tmp2;
 	qbool		classic_shotgun = cvar("k_classic_shotgun");
+	qbool       non_random_bullets = (k_yawnmode || (!match_in_progress && self && self->ct == ctPlayer && iKey(self, "nrb")));
 
 	trap_makevectors( self->s.v.v_angle );
 	VectorScale( g_globalvars.v_forward, 10, tmp );
@@ -534,7 +535,7 @@ void FireBullets( float shotcount, vec3_t dir, float spread_x, float spread_y, f
 
 	while ( shotcount > 0 )
 	{
-		if ( k_yawnmode )
+		if ( non_random_bullets )
 		{
 			if ( shotcount == 1 )
 			{
@@ -774,6 +775,7 @@ void W_FireShotgun()
 		FireBullets( bullets, dir, 0.04, 0.04, 0, dtSG );
 }
 #endif
+
 /*
 ================
 W_FireSuperShotgun
