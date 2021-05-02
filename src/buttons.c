79c405b231259224d9f42ebcfcf3dbdb30c7f7eb
Tamas Csabina <tcsabina@yahoo.com>
2021-05-02 17:52:29 +0200

    Applied code format

--- src/buttons.c
+++ src/buttons.c
@@ -24,15 +24,15 @@
  */
 
 #include "g_local.h"
-void            button_wait();
-void            button_return();
+void button_wait();
+void button_return();
 
 void button_wait()
 {
 	self->state = STATE_TOP;
 	self->s.v.nextthink = self->s.v.ltime + self->wait;
-	self->think = ( func_t ) button_return;
-	activator = PROG_TO_EDICT( self->s.v.enemy );
+	self->think = (func_t) button_return;
+	activator = PROG_TO_EDICT(self->s.v.enemy);
 
 	SUB_UseTargets();
 
@@ -48,118 +48,131 @@ void button_return()
 {
 	self->state = STATE_DOWN;
 
-	SUB_CalcMove( self->pos1, self->speed, button_done );
+	SUB_CalcMove(self->pos1, self->speed, button_done);
 
 	self->s.v.frame = 0;	// use normal textures
 
-	if ( ISLIVE(self) )
+	if (ISLIVE(self))
+	{
 		self->s.v.takedamage = DAMAGE_YES;	// can be shot again
+	}
 }
-
 
 void button_blocked()
-{// do nothing, just don't ome all the way back out
+{	// do nothing, just don't ome all the way back out
 }
-
 
 void button_fire()
 {
-	if( !k_practice ) // #practice mode#
-   	if( match_in_progress != 2 )
-       	return;
+	if (!k_practice) // #practice mode#
+	{
+		if (match_in_progress != 2)
+		{
+			return;
+		}
+	}
 
-	if ( self->state == STATE_UP || self->state == STATE_TOP )
+	if ((self->state == STATE_UP) || (self->state == STATE_TOP))
+	{
 		return;
+	}
 
-	sound( self, CHAN_VOICE, self->noise, 1, ATTN_NORM );
+	sound(self, CHAN_VOICE, self->noise, 1, ATTN_NORM);
 
 	self->state = STATE_UP;
 
-	SUB_CalcMove( self->pos2, self->speed, button_wait );
+	SUB_CalcMove(self->pos2, self->speed, button_wait);
 }
-
 
 void button_use()
 {
 	// #practice mode#
-	if (!k_practice && match_in_progress != 2) {
+	if (!k_practice && (match_in_progress != 2))
+	{
 		return;
 	}
 
-	self->s.v.enemy = EDICT_TO_PROG( activator );
+	self->s.v.enemy = EDICT_TO_PROG(activator);
 	button_fire();
 }
 
 void button_touch()
 {
 	// #practice mode#
-	if (!k_practice && match_in_progress != 2) {
+	if (!k_practice && (match_in_progress != 2))
+	{
 		return;
 	}
 
-	if ( other->ct != ctPlayer )
+	if (other->ct != ctPlayer)
+	{
 		return;
+	}
 
-	self->s.v.enemy = EDICT_TO_PROG( other );
+	self->s.v.enemy = EDICT_TO_PROG(other);
 
 	button_fire();
 }
 
 void button_killed()
 {
-	if (!k_practice && match_in_progress != 2) {
+	if (!k_practice && (match_in_progress != 2))
+	{
 		return;
 	}
 
-	if (lgc_enabled()) {
+	if (lgc_enabled())
+	{
 		return;
 	}
 
-	self->s.v.enemy = EDICT_TO_PROG( damage_attacker );
+	self->s.v.enemy = EDICT_TO_PROG(damage_attacker);
 	self->s.v.health = self->s.v.max_health;
 	self->s.v.takedamage = DAMAGE_NO;	// wil be reset upon return
 
 	button_fire();
 }
 
-
 /*QUAKED func_button (0 .5 .8) ?
-When a button is touched, it moves some distance in the direction of it's angle, triggers all of it's targets, waits some time, then returns to it's original position where it can be triggered again.
+ When a button is touched, it moves some distance in the direction of it's angle, triggers all of it's targets, waits some time, then returns to it's original position where it can be triggered again.
 
-"angle"  determines the opening direction
-"target" all entities with a matching targetname will be used
-"speed"  override the default 40 speed
-"wait"  override the default 1 second wait (-1 = never return)
-"lip"  override the default 4 pixel lip remaining at end of move
-"health" if set, the button must be killed instead of touched
-"sounds"
-0) steam metal
-1) wooden clunk
-2) metallic click
-3) in-out
-*/
+ "angle"  determines the opening direction
+ "target" all entities with a matching targetname will be used
+ "speed"  override the default 40 speed
+ "wait"  override the default 1 second wait (-1 = never return)
+ "lip"  override the default 4 pixel lip remaining at end of move
+ "health" if set, the button must be killed instead of touched
+ "sounds"
+ 0) steam metal
+ 1) wooden clunk
+ 2) metallic click
+ 3) in-out
+ */
 void SP_func_button()
 {
-	float           ftemp;
+	float ftemp;
 
-	if ( self->s.v.sounds == 0 )
+	if (self->s.v.sounds == 0)
 	{
-		trap_precache_sound( "buttons/airbut1.wav" );
+		trap_precache_sound("buttons/airbut1.wav");
 		self->noise = "buttons/airbut1.wav";
 	}
-	if ( self->s.v.sounds == 1 )
+
+	if (self->s.v.sounds == 1)
 	{
-		trap_precache_sound( "buttons/switch21.wav" );
+		trap_precache_sound("buttons/switch21.wav");
 		self->noise = "buttons/switch21.wav";
 	}
-	if ( self->s.v.sounds == 2 )
+
+	if (self->s.v.sounds == 2)
 	{
-		trap_precache_sound( "buttons/switch02.wav" );
+		trap_precache_sound("buttons/switch02.wav");
 		self->noise = "buttons/switch02.wav";
 	}
-	if ( self->s.v.sounds == 3 )
+
+	if (self->s.v.sounds == 3)
 	{
-		trap_precache_sound( "buttons/switch04.wav" );
+		trap_precache_sound("buttons/switch04.wav");
 		self->noise = "buttons/switch04.wav";
 	}
 
@@ -169,33 +182,42 @@ void SP_func_button()
 	// if button does't have model ( mapper fault? ),
 	// we must set it to something safe, because with SOLID_BSP and null model server crashed
 	self->s.v.solid = (strnull(self->model) ? SOLID_NOT : SOLID_BSP);
-	setmodel( self, self->model );
+	setmodel(self, self->model);
 
-	self->blocked = ( func_t ) button_blocked;
-	self->use = ( func_t ) button_use;
+	self->blocked = (func_t) button_blocked;
+	self->use = (func_t) button_use;
 
-	if ( ISLIVE(self) )
+	if (ISLIVE(self))
 	{
 		self->s.v.max_health = self->s.v.health;
 		self->th_die = button_killed;
 		self->s.v.takedamage = DAMAGE_YES;
-	} else
-		self->touch = ( func_t ) button_touch;
+	}
+	else
+	{
+		self->touch = (func_t) button_touch;
+	}
 
-	if ( !self->speed )
+	if (!self->speed)
+	{
 		self->speed = 40;
+	}
 
-	if ( !self->wait )
+	if (!self->wait)
+	{
 		self->wait = 1;
+	}
 
-	if ( !self->lip )
+	if (!self->lip)
+	{
 		self->lip = 4;
+	}
 
 	self->state = STATE_BOTTOM;
 
 // self->pos1 = self->s.v.origin;
-	VectorCopy( self->s.v.origin, self->pos1 );
-	ftemp = ( fabs( DotProduct( self->s.v.movedir, self->s.v.size ) ) - self->lip );
+	VectorCopy(self->s.v.origin, self->pos1);
+	ftemp = (fabs(DotProduct(self->s.v.movedir, self->s.v.size)) - self->lip);
 
 	self->pos2[0] = self->pos1[0] + self->s.v.movedir[0] * ftemp;
 	self->pos2[1] = self->pos1[1] + self->s.v.movedir[1] * ftemp;
