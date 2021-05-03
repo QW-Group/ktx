e28b31d759c3a730bf32140d70c806769bfd1cd4
Tamas Csabina <tcsabina@yahoo.com>
2021-05-03 13:50:49 +0200

    Applied code format

--- src/fb_globals.h
+++ src/fb_globals.h
@@ -15,37 +15,39 @@
 #define FB_OPTION_DEBUG_MOVEMENT       128
 #define FB_OPTION_SHOW_THINKING     (FB_OPTION_SHOW_DUEL_LOGIC | FB_OPTION_SHOW_GOAL_LOGIC | FB_OPTION_SHOW_ROUTING_LOGIC | FB_OPTION_SHOW_MOVEMENT_LOGIC)
 
-typedef void (*fb_spawn_funcref_t)(gedict_t* ent);
+typedef void (*fb_spawn_funcref_t)(gedict_t *ent);
 
-typedef struct fb_spawn_s {
-	char* name;                 // classname
+typedef struct fb_spawn_s
+{
+	char *name;                 // classname
 	fb_spawn_funcref_t func;       // initialisation function
 } fb_spawn_t;
 
-typedef struct fb_path_eval_s {
-	gedict_t* touch_marker;
-	gedict_t* test_marker;
+typedef struct fb_path_eval_s
+{
+	gedict_t *touch_marker;
+	gedict_t *test_marker;
 	qbool rocket_alert;
 	int description;
 	float path_time;
 	qbool path_normal;
 	vec3_t player_origin;
-	gedict_t* goalentity_marker;
+	gedict_t *goalentity_marker;
 	float goal_late_time;
 	float lookahead_time_;
 	vec3_t player_direction;
 	qbool be_quiet;
-	gedict_t* player;
+	gedict_t *player;
 } fb_path_eval_t;
 
 // Globals used for general path-finding
 extern qbool path_normal;
-extern gedict_t* look_marker;
-extern gedict_t* from_marker;
-extern gedict_t* middle_marker;
-extern gedict_t* to_marker;
-extern gedict_t* next_marker;
-extern gedict_t* prev_marker;
+extern gedict_t *look_marker;
+extern gedict_t *from_marker;
+extern gedict_t *middle_marker;
+extern gedict_t *to_marker;
+extern gedict_t *next_marker;
+extern gedict_t *prev_marker;
 extern float traveltime;
 extern float look_traveltime;
 extern float zone_time;
@@ -58,7 +60,7 @@ extern float half_sv_maxspeed;
 extern float inv_sv_maxspeed;
 extern float sv_accelerate;
 
-extern gedict_t* dropper;
+extern gedict_t *dropper;
 
 // FIXME: A lot of these not used anymore
 #define FL_ONGROUND_PARTIALGROUND (FL_ONGROUND | FL_PARTIALGROUND)
@@ -162,227 +164,231 @@ extern gedict_t* dropper;
 
 #define HELP_TEAMMATE 128   // FIXME: same as AWARE_SURROUNDINGS... deliberate?
 
-qbool ExistsPath (gedict_t* from_marker, gedict_t* to_marker, int* new_path_state);
-float boomstick_only (void);
+qbool ExistsPath(gedict_t *from_marker, gedict_t *to_marker, int *new_path_state);
+float boomstick_only(void);
 
-float CountTeams (void);
-qbool EnemyDefenceless (gedict_t* self);
+float CountTeams(void);
+qbool EnemyDefenceless(gedict_t *self);
 
-qbool enemy_shaft_attack (gedict_t* self, gedict_t* enemy);
-float W_BestWeapon (void);
+qbool enemy_shaft_attack(gedict_t *self, gedict_t *enemy);
+float W_BestWeapon(void);
 
-gedict_t* LocateMarker (vec3_t org);
-gedict_t* LocateNextMarker (vec3_t org, gedict_t* ignore_ent);
-float RankForWeapon (float w);
-float WeaponCode (float w);
+gedict_t* LocateMarker(vec3_t org);
+gedict_t* LocateNextMarker(vec3_t org, gedict_t *ignore_ent);
+float RankForWeapon(float w);
+float WeaponCode(float w);
 
-float crandom (void);
+float crandom(void);
 
-void BotCanRocketJump (gedict_t* self);
-qbool VisibleEntity (gedict_t* ent);
-gedict_t* IdentifyMostVisibleTeammate (gedict_t* self);
-float anglemod (float v);
-gedict_t* HelpTeammate (void);
-float TotalStrengthAfterDamage (float health, float armorValue, float armorType, float damage);
-float TotalStrength (float health, float armorValue, float armorType);
+void BotCanRocketJump(gedict_t *self);
+qbool VisibleEntity(gedict_t *ent);
+gedict_t* IdentifyMostVisibleTeammate(gedict_t *self);
+float anglemod(float v);
+gedict_t* HelpTeammate(void);
+float TotalStrengthAfterDamage(float health, float armorValue, float armorType, float damage);
+float TotalStrength(float health, float armorValue, float armorType);
 
 // 
 char* BotNameGeneric(int botNumber);
 char* BotNameFriendly(int botNumber);
 char* BotNameEnemy(int botNumber);
 
-qbool Visible_360(gedict_t* self, gedict_t* visible_object);
-qbool Visible_infront(gedict_t* self, gedict_t* visible_object);
-unsigned int ClientFlag(gedict_t* client);
+qbool Visible_360(gedict_t *self, gedict_t *visible_object);
+qbool Visible_infront(gedict_t *self, gedict_t *visible_object);
+unsigned int ClientFlag(gedict_t *client);
 
 // marker_util.qc
 void marker_touch(void);
-void check_marker(gedict_t* self, gedict_t* other);
-void BecomeMarker(gedict_t* marker);
+void check_marker(gedict_t *self, gedict_t *other);
+void BecomeMarker(gedict_t *marker);
 
 // route_calc.qc
-void CheckWaterColumn(gedict_t* m, vec3_t m_pos, vec3_t testplace);
+void CheckWaterColumn(gedict_t *m, vec3_t m_pos, vec3_t testplace);
 
 // route_fields.qc
 gedict_t* CreateNewMarker(vec3_t origin);
 
 // botwater.qc
-void BotWaterMove(gedict_t* self);
+void BotWaterMove(gedict_t *self);
 void BotWaterJumpFix(void);
 
 // items.qc
-float goal_NULL(gedict_t* self, gedict_t* other);
+float goal_NULL(gedict_t *self, gedict_t *other);
 
 // client.qc
-void BotClientEntersEvent(gedict_t* self, gedict_t* spawn_pos);
-void BotPreThink(gedict_t* self);
-void BotClientConnectedEvent(gedict_t* self);
-void BotOutOfWater(gedict_t* self);
-void BotSetCommand(gedict_t* self);
-void BotsThinkTime(gedict_t* self);
+void BotClientEntersEvent(gedict_t *self, gedict_t *spawn_pos);
+void BotPreThink(gedict_t *self);
+void BotClientConnectedEvent(gedict_t *self);
+void BotOutOfWater(gedict_t *self);
+void BotSetCommand(gedict_t *self);
+void BotsThinkTime(gedict_t *self);
 
 // botphys.qc
-void FrogbotPrePhysics1 (void);
-void FrogbotPrePhysics2 (void);
+void FrogbotPrePhysics1(void);
+void FrogbotPrePhysics2(void);
 
 // bothazd.qc
-void AvoidHazards (gedict_t* self);
-void NewVelocityForArrow (gedict_t* self, vec3_t dir_move, const char* explanation);
+void AvoidHazards(gedict_t *self);
+void NewVelocityForArrow(gedict_t *self, vec3_t dir_move, const char *explanation);
 
 // route_lookup.qc
-gedict_t* SightMarker (gedict_t* from_marker, gedict_t* to_marker, float max_distance, float min_height_diff);
-gedict_t* HigherSightFromFunction (gedict_t* from_marker, gedict_t* to_marker);
-gedict_t* SightFromMarkerFunction (gedict_t* from_marker, gedict_t* to_marker);
-gedict_t* SubZoneNextPathMarker (gedict_t* from_marker, gedict_t* to_marker);
-float SubZoneArrivalTime (float zone_time, gedict_t* middle_marker, gedict_t* to_marker, qbool rl_routes);
-float SightFromTime (gedict_t* from_marker, gedict_t* to_marker);
-void ZoneMarker (gedict_t* from_marker, gedict_t* to_marker, qbool path_normal, qbool rj_routes);
-gedict_t* ZonePathMarker (gedict_t* from_marker, gedict_t* to_marker, qbool path_normal, qbool rl_jump_routes);
+gedict_t* SightMarker(gedict_t *from_marker, gedict_t *to_marker, float max_distance,
+						float min_height_diff);
+gedict_t* HigherSightFromFunction(gedict_t *from_marker, gedict_t *to_marker);
+gedict_t* SightFromMarkerFunction(gedict_t *from_marker, gedict_t *to_marker);
+gedict_t* SubZoneNextPathMarker(gedict_t *from_marker, gedict_t *to_marker);
+float SubZoneArrivalTime(float zone_time, gedict_t *middle_marker, gedict_t *to_marker,
+							qbool rl_routes);
+float SightFromTime(gedict_t *from_marker, gedict_t *to_marker);
+void ZoneMarker(gedict_t *from_marker, gedict_t *to_marker, qbool path_normal, qbool rj_routes);
+gedict_t* ZonePathMarker(gedict_t *from_marker, gedict_t *to_marker, qbool path_normal,
+							qbool rl_jump_routes);
 
 // botweap.qc
-void FrogbotSetFirepower (gedict_t* self);
-void SelectWeapon (void);
-void SetFireButton (gedict_t* self, vec3_t rel_pos, float rel_dist);
-void FrogbotWeaponFiredEvent (gedict_t* self);
-void FindRocketExplosionPoint(vec3_t player_origin, vec3_t player_angles, vec3_t output, float* distance_frac);
-qbool AttackFinished(gedict_t* self);
+void FrogbotSetFirepower(gedict_t *self);
+void SelectWeapon(void);
+void SetFireButton(gedict_t *self, vec3_t rel_pos, float rel_dist);
+void FrogbotWeaponFiredEvent(gedict_t *self);
+void FindRocketExplosionPoint(vec3_t player_origin, vec3_t player_angles, vec3_t output,
+								float *distance_frac);
+qbool AttackFinished(gedict_t *self);
 
 // marker_util.qc
-void AssignVirtualGoal (gedict_t* item);
-void AssignVirtualGoal_apply (gedict_t* marker_);
-void adjust_view_ofs_z (gedict_t* ent);
+void AssignVirtualGoal(gedict_t *item);
+void AssignVirtualGoal_apply(gedict_t *marker_);
+void adjust_view_ofs_z(gedict_t *ent);
 
 // botenemy.qc
-void ClearLookObject (gedict_t* client);
-void LookEnemy (gedict_t* player, gedict_t* enemy);
-qbool BotsPickBestEnemy (gedict_t* self);
-void BotDamageInflictedEvent (gedict_t* attacker, gedict_t* targ);
+void ClearLookObject(gedict_t *client);
+void LookEnemy(gedict_t *player, gedict_t *enemy);
+qbool BotsPickBestEnemy(gedict_t *self);
+void BotDamageInflictedEvent(gedict_t *attacker, gedict_t *targ);
 
 // botjump.qc
-void CheckCombatJump (gedict_t* self);
+void CheckCombatJump(gedict_t *self);
 //void BotInLava(void);
-void BotPerformRocketJump (gedict_t* self);
+void BotPerformRocketJump(gedict_t *self);
 
 // botgoal.qc
-void UpdateGoal (gedict_t* self);
+void UpdateGoal(gedict_t *self);
 
 // botpath.qc
-void ProcessNewLinkedMarker (gedict_t* self);
+void ProcessNewLinkedMarker(gedict_t *self);
 
 // marker_load.qc
-gedict_t* CreateMarker (float x, float y, float z);
-void AllMarkersLoaded (void);
+gedict_t* CreateMarker(float x, float y, float z);
+void AllMarkersLoaded(void);
 
-void SetZone (int zone_number, int marker_number);
-void SetMarkerFlag (int marker_number, int flags);
-void SetMarkerFixedSize (int marker_number, int size_x, int size_y, int size_z);
-void AddToQue (gedict_t* ent);
-void SetGoal (int goal, int marker_number);
-void SetGoalForMarker (int goal, gedict_t* marker);
-void SetMarkerPathFlags (int marker_number, int path_index, int flags);
-void SetMarkerPath (int source_marker, int path_index, int next_marker);
-void SetMarkerViewOffset (int marker, float zOffset);
+void SetZone(int zone_number, int marker_number);
+void SetMarkerFlag(int marker_number, int flags);
+void SetMarkerFixedSize(int marker_number, int size_x, int size_y, int size_z);
+void AddToQue(gedict_t *ent);
+void SetGoal(int goal, int marker_number);
+void SetGoalForMarker(int goal, gedict_t *marker);
+void SetMarkerPathFlags(int marker_number, int path_index, int flags);
+void SetMarkerPath(int source_marker, int path_index, int next_marker);
+void SetMarkerViewOffset(int marker, float zOffset);
 
 #define FROGBOT_PATH_FLAG_OPTIONS "w6rjva"
 #define FROGBOT_MARKER_FLAG_OPTIONS "u6fbte"
 
 // added for ktx
-qbool fb_lg_disabled (void);
+qbool fb_lg_disabled(void);
 //void StartItems(void);
 
 // maps
-void LoadMap (void);
-qbool FrogbotsCheckMapSupport (void);
+void LoadMap(void);
+qbool FrogbotsCheckMapSupport(void);
 
-void map_aerowalk (void);
-void map_amphi2 (void);
-void map_dm4 (void);
-void map_dm3 (void);
-void map_dm6 (void);
-void map_e1m2 (void);
-void map_frobodm2 (void);
-void map_pkeg1 (void);
-void map_povdmm4 (void);
-void map_spinev2 (void);
-void map_ukooldm2 (void);
-void map_ukooldm3 (void);
-void map_ukooldm6 (void);
-void map_ukooldm8 (void);
-void map_ultrav (void);
-void map_ztndm1 (void);
-void map_ztndm2 (void);
-void map_ztndm3 (void);
-void map_ztndm4 (void);
-void map_ztndm5 (void);
-void map_ztndm6 (void);
-
-// 
-float pr1_rint (float f);
+void map_aerowalk(void);
+void map_amphi2(void);
+void map_dm4(void);
+void map_dm3(void);
+void map_dm6(void);
+void map_e1m2(void);
+void map_frobodm2(void);
+void map_pkeg1(void);
+void map_povdmm4(void);
+void map_spinev2(void);
+void map_ukooldm2(void);
+void map_ukooldm3(void);
+void map_ukooldm6(void);
+void map_ukooldm8(void);
+void map_ultrav(void);
+void map_ztndm1(void);
+void map_ztndm2(void);
+void map_ztndm3(void);
+void map_ztndm4(void);
+void map_ztndm5(void);
+void map_ztndm6(void);
 
 // 
-void BotEvadeLogic (gedict_t* self);
-qbool SameTeam (gedict_t* p1, gedict_t* p2);
+float pr1_rint(float f);
+
+// 
+void BotEvadeLogic(gedict_t *self);
+qbool SameTeam(gedict_t *p1, gedict_t *p2);
 
 // botstat.qc
-void FrogbotSetHealthArmour (gedict_t* client);
-void UpdateGoalEntity (gedict_t* item, gedict_t* taker);
+void FrogbotSetHealthArmour(gedict_t *client);
+void UpdateGoalEntity(gedict_t *item, gedict_t *taker);
 
 // bot_client.qc
-void BotPlayerDeathEvent (gedict_t* self);
-qbool BotUsingCorrectWeapon (gedict_t* self);
+void BotPlayerDeathEvent(gedict_t *self);
+qbool BotUsingCorrectWeapon(gedict_t *self);
 
 // bot_items.qc
-int ItemSpawnFunctionCount (void);
-void StartItemFB (gedict_t* ent);
-qbool NoItemTouch (gedict_t* self, gedict_t* other);
-qbool BotsPreTeleport (gedict_t* self, gedict_t* other);
+int ItemSpawnFunctionCount(void);
+void StartItemFB(gedict_t *ent);
+qbool NoItemTouch(gedict_t *self, gedict_t *other);
+qbool BotsPreTeleport(gedict_t *self, gedict_t *other);
 
 // FBCA code just set nextthink to 0 when item respawned...
-qbool WaitingToRespawn (gedict_t* ent);
+qbool WaitingToRespawn(gedict_t *ent);
 
-int NumberOfClients (void);
-qbool TimeTrigger (float *next_time, float time_increment);
+int NumberOfClients(void);
+qbool TimeTrigger(float *next_time, float time_increment);
 
-qbool IsMarkerFrame (void);
-qbool IsHazardFrame (void);
+qbool IsMarkerFrame(void);
+qbool IsHazardFrame(void);
 
 // fb_globals.c
-gedict_t* FirstZoneMarker (int zone);
-void AddZoneMarker (gedict_t* marker);
+gedict_t* FirstZoneMarker(int zone);
+void AddZoneMarker(gedict_t *marker);
 
-void ResetGoalEntity(gedict_t* self);
+void ResetGoalEntity(gedict_t *self);
 
 // bot_commands.qc
-void SetLinkedMarker (gedict_t* player, gedict_t* marker, char* explanation);
-void SetJumpFlag (gedict_t* player, qbool jumping, const char* explanation);
+void SetLinkedMarker(gedict_t *player, gedict_t *marker, char *explanation);
+void SetJumpFlag(gedict_t *player, qbool jumping, const char *explanation);
 
 // bot_routing.qc
-void PathScoringLogic (
-	float goal_respawn_time, qbool be_quiet, float lookahead_time, qbool path_normal, vec3_t player_origin, vec3_t player_direction, gedict_t* touch_marker_,
-	gedict_t* goalentity_marker, qbool rocket_alert, qbool rocket_jump_routes_allowed,
-	qbool trace_bprint, gedict_t* player,
-	float *best_score, gedict_t** linked_marker_, int* new_path_state, int* new_angle_hint, int* new_rj_frame_delay, float new_rj_angles[2]
-);
+void PathScoringLogic(float goal_respawn_time, qbool be_quiet, float lookahead_time,
+						qbool path_normal, vec3_t player_origin, vec3_t player_direction,
+						gedict_t *touch_marker_, gedict_t *goalentity_marker, qbool rocket_alert,
+						qbool rocket_jump_routes_allowed, qbool trace_bprint, gedict_t *player,
+						float *best_score, gedict_t **linked_marker_, int *new_path_state,
+						int *new_angle_hint, int *new_rj_frame_delay, float new_rj_angles[2]);
 
-int BotVersionNumber (void);
-qbool FrogbotOptionEnabled (int option);
-qbool FrogbotShowMarkerIndicators (void);
+int BotVersionNumber(void);
+qbool FrogbotOptionEnabled(int option);
+qbool FrogbotShowMarkerIndicators(void);
 
-void SetDirectionMove (gedict_t* self, vec3_t dir_move, const char* explanation);
+void SetDirectionMove(gedict_t *self, vec3_t dir_move, const char *explanation);
 
-void BotEventPlatformHitTop (gedict_t* self);
-void BotEventPlatformHitBottom (gedict_t* self);
-void BotEventDoorHitTop (gedict_t* self);
-void BotEventDoorHitBottom (gedict_t* self);
-void BotPlatformTouched (gedict_t* platform, gedict_t* player);
+void BotEventPlatformHitTop(gedict_t *self);
+void BotEventPlatformHitBottom(gedict_t *self);
+void BotEventDoorHitTop(gedict_t *self);
+void BotEventDoorHitBottom(gedict_t *self);
+void BotPlatformTouched(gedict_t *platform, gedict_t *player);
 
-void BotsBackpackTouchedNonPlayer (gedict_t* backpack, gedict_t* entity);
-void BotsMatchStart (void);
-void BotsAssignTeamFlags (void);
-qbool HasWeapon (gedict_t* player, int weapon);
+void BotsBackpackTouchedNonPlayer(gedict_t *backpack, gedict_t *entity);
+void BotsMatchStart(void);
+void BotsAssignTeamFlags(void);
+qbool HasWeapon(gedict_t *player, int weapon);
 
 // botthink.qc
-void SetMarker (gedict_t* client, gedict_t* marker);
+void SetMarker(gedict_t *client, gedict_t *marker);
 
 #define PAST(x) (g_globalvars.time >= self->fb.x)
 #define FUTURE(x) (g_globalvars.time < self->fb.x)
@@ -396,14 +402,14 @@ void SetMarker (gedict_t* client, gedict_t* marker);
 
 #define FB_MAPDEATHHEIGHT_DEFAULT -9999999
 
-int MapDeathHeight (void);
-void SetMapDeathHeight (int height);
+int MapDeathHeight(void);
+void SetMapDeathHeight(int height);
 
 // debugging
-void RunRandomTrials (float min, float max, float mult);
+void RunRandomTrials(float min, float max, float mult);
 
 // editor
-qbool HasSavedMarker (void);
+qbool HasSavedMarker(void);
 
 #define FB_CVAR_ENABLED       "k_fb_enabled"
 #define FB_CVAR_OPTIONS       "k_fb_options"
