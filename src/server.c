25f95a82a7d6cf1ebbb07f2256ea1e412c230f88
Tamas Csabina <tcsabina@yahoo.com>
2021-05-10 21:24:09 +0200

    Applied code format

--- src/server.c
+++ src/server.c
@@ -26,91 +26,104 @@
 #include "g_local.h"
 
 /*
-==============================================================================
+ ==============================================================================
 
-MOVETARGET CODE
+ MOVETARGET CODE
 
-The angle of the movetarget effects standing and bowing direction, but has no effect on movement, which allways heads to the next target.
+ The angle of the movetarget effects standing and bowing direction, but has no effect on movement, which allways heads to the next target.
 
-targetname
-must be present.  The name of this movetarget.
+ targetname
+ must be present.  The name of this movetarget.
 
-target
-the next spot to move to.  If not present, stop here for good.
+ target
+ the next spot to move to.  If not present, stop here for good.
 
-pausetime
-The number of seconds to spend standing or bowing for path_stand or path_bow
+ pausetime
+ The number of seconds to spend standing or bowing for path_stand or path_bow
 
-==============================================================================
-*/
+ ==============================================================================
+ */
 
 /*
-=============
-t_movetarget
+ =============
+ t_movetarget
 
-Something has bumped into a movetarget.  If it is a monster
-moving towards it, change the next destination and continue.
-==============
-*/
+ Something has bumped into a movetarget.  If it is a monster
+ moving towards it, change the next destination and continue.
+ ==============
+ */
 void t_movetarget()
 {
-	gedict_t       *temp;
-	vec3_t          tmpv;
+	gedict_t *temp;
+	vec3_t tmpv;
 
-	if ( other->movetarget != self )
+	if (other->movetarget != self)
+	{
 		return;
+	}
 
-	if ( other->s.v.enemy )
+	if (other->s.v.enemy)
+	{
 		return;		// fighting, not following a path
+	}
 
 	temp = self;
 	self = other;
 	other = temp;
 
-	if ( streq( self->classname, "monster_ogre" ) )
-		sound( self, CHAN_VOICE, "ogre/ogdrag.wav", 1, ATTN_IDLE );	// play chainsaw drag sound
+	if (streq(self->classname, "monster_ogre"))
+	{
+		sound(self, CHAN_VOICE, "ogre/ogdrag.wav", 1, ATTN_IDLE);	// play chainsaw drag sound
+	}
 
-//dprint ("t_movetarget\n");
-	if ( other->target )
-		self->movetarget = find( world, FOFS( targetname ), other->target );
+//	dprint ("t_movetarget\n");
+	if (other->target)
+	{
+		self->movetarget = find(world, FOFS(targetname), other->target);
+	}
 	else
+	{
 		self->movetarget = NULL;
+	}
 
-	if ( !self->movetarget ) // NOTE: this is a damn difference with qc
+	if (!self->movetarget) // NOTE: this is a damn difference with qc
+	{
 		self->movetarget = world;
-	self->s.v.goalentity = EDICT_TO_PROG( self->movetarget );
+	}
 
-	VectorSubtract( self->movetarget->s.v.origin, self->s.v.origin, tmpv );
-	self->s.v.ideal_yaw = vectoyaw( tmpv );
-	if ( !self->movetarget || self->movetarget == world )
+	self->s.v.goalentity = EDICT_TO_PROG(self->movetarget);
+
+	VectorSubtract(self->movetarget->s.v.origin, self->s.v.origin, tmpv);
+	self->s.v.ideal_yaw = vectoyaw(tmpv);
+	if (!self->movetarget || (self->movetarget == world))
 	{
 		self->pausetime = g_globalvars.time + 999999;
-		if ( self->th_stand )
+		if (self->th_stand)
+		{
 			self->th_stand(); //monster code
+		}
+
 		return;
 	}
 }
 
-
 void movetarget_f()
 {
-	if ( !self->targetname )
-		G_Error( "monster_movetarget: no targetname" );
+	if (!self->targetname)
+	{
+		G_Error("monster_movetarget: no targetname");
+	}
 
 	self->s.v.solid = SOLID_TRIGGER;
-	self->touch = ( func_t ) t_movetarget;
-	setsize( self, -8, -8, -8, 8, 8, 8 );
+	self->touch = (func_t) t_movetarget;
+	setsize(self, -8, -8, -8, 8, 8, 8);
 
 }
 
 /*QUAKED path_corner (0.5 0.3 0) (-8 -8 -8) (8 8 8)
-Monsters will continue walking towards the next target corner.
-*/
+ Monsters will continue walking towards the next target corner.
+ */
 void SP_path_corner()
 {
 	movetarget_f();
 }
-
-
-
-//============================================================================
