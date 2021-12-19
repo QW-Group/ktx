8424e4d17253b2cc87537ad5622e56f684268a1c
Ivan Bolsunov <ivan.bolsunov@gmail.com>
2021-12-19 12:27:03 +0300

    BUILD: add CMake build

---
+++ build_cmake.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Useful if you willing to stop on first error, also print what is executed.
+#set -ex
+
+BUILDIR="${BUILDIR:-build}" # Default build dir.
+
+# Default bot support, CMake already have it as ON,
+# provided here just for possibility to turn it OFF without modification of CMake or this script file.
+BOT_SUPPORT="${BOT_SUPPORT:-ON}"
+
+# If V variable is not empty then provide -v argument to Ninja (verbose output).
+V="${V:-}"
+[ ! -z ${V} ] && V="-v"
+
+rm -rf ${BUILDIR}
+mkdir -p ${BUILDIR}
+
+# Define target platforms, feel free to comment out if you does not require some of it.
+BUILD_LIST=(
+    linux-amd64
+    linux-armhf
+    linux-i686
+    windows-x64
+    windows-x86
+    qvm
+)
+
+# Build platforms one by one.
+for name in "${BUILD_LIST[@]}"; do
+    mkdir -p ${BUILDIR}/$name
+    case "$name" in
+    "qvm" ) # Build QVM library.
+        cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} -G Ninja
+        cmake --build ${BUILDIR}/$name --config Release --target qvm ${V}
+    ;;
+    * ) # Build native library.
+        cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} -G Ninja -DCMAKE_TOOLCHAIN_FILE=tools/cross-cmake/$name.cmake
+        cmake --build ${BUILDIR}/$name --config Release ${V}
+    ;;
+    esac
+done
---
+++ tools/cross-cmake/linux-amd64.cmake
@@ -0,0 +1,13 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
