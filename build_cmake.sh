0c33e66c8464b8b40cf914617e9a8c8d71425c70
ciscon <ciscon@nicotinelounge.com>
2021-12-20 18:47:13 -0500

    BUILD: if ninja not available allow cmake to fallback to default generator

--- build_cmake.sh
+++ build_cmake.sh
@@ -18,26 +18,33 @@ mkdir -p ${BUILDIR}
 
 # Define target platforms, feel free to comment out if you does not require some of it.
 BUILD_LIST=(
-    linux-amd64
-    linux-aarch64
-    linux-armhf
-    linux-i686
-    windows-x64
-    windows-x86
-    qvm
+	linux-amd64
+	linux-aarch64
+	linux-armhf
+	linux-i686
+	windows-x64
+	windows-x86
+	qvm
 )
+
+# Either use default CMake generator (most of the time its make) or ninja if found.
+CMAKE_GENERATOR=
+if hash ninja >/dev/null 2>&1
+then
+	CMAKE_GENERATOR="-G Ninja"
+fi
 
 # Build platforms one by one.
 for name in "${BUILD_LIST[@]}"; do
-    mkdir -p ${BUILDIR}/$name
-    case "$name" in
-    "qvm" ) # Build QVM library.
-        cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} -G Ninja
-        cmake --build ${BUILDIR}/$name --config Release --target qvm ${V}
-    ;;
-    * ) # Build native library.
-        cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} -G Ninja -DCMAKE_TOOLCHAIN_FILE=tools/cross-cmake/$name.cmake
-        cmake --build ${BUILDIR}/$name --config Release ${V}
-    ;;
-    esac
+	mkdir -p ${BUILDIR}/$name
+	case "$name" in
+	"qvm" ) # Build QVM library.
+		cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} ${CMAKE_GENERATOR}
+		cmake --build ${BUILDIR}/$name --config Release --target qvm ${V}
+	;;
+	* ) # Build native library.
+		cmake -B ${BUILDIR}/$name -S . -DBOT_SUPPORT=${BOT_SUPPORT} ${CMAKE_GENERATOR} -DCMAKE_TOOLCHAIN_FILE=tools/cross-cmake/$name.cmake
+		cmake --build ${BUILDIR}/$name --config Release ${V}
+	;;
+	esac
 done
