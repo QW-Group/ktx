046401caac406e41af86e81e6c6cb8678c1d17ff
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2022-02-01 17:44:52 -0800

    CLAN ARENA: pauses before and after rounds

--- src/clan_arena.c
+++ src/clan_arena.c
@@ -7,6 +7,9 @@
 static int round_num;
 static int team1_score;
 static int team2_score;
+static int pause_count;
+static int pause_time;
+static int round_pause;
 
 qbool is_rules_change_allowed(void);
 
@@ -110,10 +113,10 @@ void CA_PutClientInServer(void)
 	{
 		int items;
 
-		self->s.v.ammo_nails = 120;
-		self->s.v.ammo_shells = 40;
-		self->s.v.ammo_rockets = 20;
-		self->s.v.ammo_cells = 35;
+		self->s.v.ammo_nails = 200;
+		self->s.v.ammo_shells = 100;
+		self->s.v.ammo_rockets = 60;
+		self->s.v.ammo_cells = 150;
 
 		self->s.v.armorvalue = 200;
 		self->s.v.armortype = 0.8;
@@ -252,6 +255,58 @@ void CA_TeamsStats(void)
 	}
 }
 
+void EndRound(int alive_team)
+{
+	if(!round_pause)
+	{
+		round_pause = 1;
+		pause_time = g_globalvars.time + 6;
+
+		if (alive_team == 1)
+		{
+			team1_score++;
+		}
+		else
+		{
+			team2_score++;
+		}
+	}
+
+	pause_count = Q_rint(pause_time - g_globalvars.time);
+
+	if (pause_count <= 0)
+	{
+		round_pause = 0;
+		round_num++;
+		ra_match_fight = 0;
+		G_cp2all("round %d", round_num);
+	}
+	else 
+	{
+		if (pause_count < 6)
+		{
+			if (alive_team == 0)
+			{
+				//G_bprint(2, "%s\n", redtext("draw"));
+				G_cp2all("Round draw!");
+			}
+			
+			else
+			{
+				//G_bprint(2, "%s \x90%s\x91 wins round\n", redtext("Team"),
+				//				cvar_string(va("_k_team%d", alive_team)));
+				
+				G_cp2all("Team \x90%s\x91 wins the round!",
+						cvar_string(va("_k_team%d", alive_team))); // CA_wins_required
+			}
+		}
+		if (pause_count < 5)
+		{
+			ra_match_fight = 1; // disable firing
+		}
+	}
+}
+
 void CA_Frame(void)
 {
 	static int last_r;
@@ -269,31 +324,18 @@ void CA_Frame(void)
 	}
 
 	// check if there exist only one team with alive players and others are eluminated, if so then its time to start ca countdown
-	if (ra_match_fight == 2)
+	if (ra_match_fight == 2 || (ra_match_fight == 1 && round_pause == 1))
 	{
 		int alive_team = 0;
 
 		switch (CA_check_alive_teams(&alive_team))
 		{
 			case 0: // DRAW, both teams are dead
-				G_bprint(2, "%s\n", redtext("draw"));
-				round_num++;
-				ra_match_fight = 0;
+				EndRound(0);
 				break;
 
 			case 1: // Only one team alive
-				G_bprint(2, "%s \x90%s\x91 wins round\n", redtext("Team"),
-							cvar_string(va("_k_team%d", alive_team)));
-				if (alive_team == 1)
-				{
-					team1_score++;
-				}
-				else
-				{
-					team2_score++;
-				}
-				round_num++;
-				ra_match_fight = 0;
+				EndRound(alive_team);
 				break;
 
 			default:
@@ -313,54 +355,57 @@ void CA_Frame(void)
 		return;
 	}
 
-	if (!ra_match_fight)
+	if (!round_pause)
 	{
-		// ok start ra timer
-		ra_match_fight = 1; // ra countdown
-		last_r = 999999999;
-		time_to_start = g_globalvars.time + 6;
-
-		for (p = world; (p = find_plr(p));)
+		if (!ra_match_fight)
 		{
-			k_respawn(p, false);
-		}
-	}
+			// ok start ra timer
+			ra_match_fight = 1; // ra countdown
+			last_r = 999999999;
+			time_to_start = g_globalvars.time + 9;
 
-	r = Q_rint(time_to_start - g_globalvars.time);
-
-	if (r <= 0)
-	{
-		char *fight = redtext("FIGHT!");
-
-		// TODO replace RA sounds (but world precached)
-		//sound (world, CHAN_AUTO + CHAN_NO_PHS_ADD, "ra/fight.wav", 1, ATTN_NONE);
-		G_cp2all("%s", fight);
-
-		ra_match_fight = 2;
-
-		// rounding suck, so this force readytostart() return true right after FIGHT! is printed
-		time_to_start = g_globalvars.time;
-	}
-	else if (r != last_r)
-	{
-		last_r = r;
-
-		if (r < 6)
-		{
-			// TODO replace RA sounds (but world precached)
-			//sound (world, CHAN_AUTO + CHAN_NO_PHS_ADD, va("ra/%d.wav", r), 1, ATTN_NONE);
-			for (p = world; (p = find_client(p));)
+			for (p = world; (p = find_plr(p));)
 			{
-				stuffcmd(p, "play buttons/switch04.wav\n");
+				k_respawn(p, false);
 			}
 		}
 
-		if (r < 11)
+		r = Q_rint(time_to_start - g_globalvars.time);
+
+		if (r <= 0)
 		{
-			G_cp2all("%d\n\n"
-						"\x90%s\x91:%s \x90%s\x91:%s",
-						r, cvar_string("_k_team1"), dig3(team1_score), cvar_string("_k_team2"),
-						dig3(team2_score)); // CA_wins_required
+			char *fight = redtext("FIGHT!");
+
+			// TODO replace RA sounds (but world precached)
+			//sound (world, CHAN_AUTO + CHAN_NO_PHS_ADD, "ra/fight.wav", 1, ATTN_NONE);
+			G_cp2all("%s", fight);
+
+			ra_match_fight = 2;
+
+			// rounding suck, so this force readytostart() return true right after FIGHT! is printed
+			time_to_start = g_globalvars.time;
+		}
+		else if (r != last_r)
+		{
+			last_r = r;
+
+			if (r < 6)
+			{
+				// TODO replace RA sounds (but world precached)
+				//sound (world, CHAN_AUTO + CHAN_NO_PHS_ADD, va("ra/%d.wav", r), 1, ATTN_NONE);
+				for (p = world; (p = find_client(p));)
+				{
+					stuffcmd(p, "play buttons/switch04.wav\n");
+				}
+			}
+
+			if (r < 6) // was 11
+			{
+				G_cp2all("%d\n\n"
+							"\x90%s\x91:%s \x90%s\x91:%s",
+							r, cvar_string("_k_team1"), dig3(team1_score), cvar_string("_k_team2"),
+							dig3(team2_score)); // CA_wins_required
+			}
 		}
 	}
 }
