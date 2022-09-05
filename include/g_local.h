883d73b8b231e5e3816f816b25d2879ee3ed7d46
Tamas Csabina <tcsabina@yahoo.com>
2022-09-06 00:26:31 +0200

    Starting 1.42 dev

--- include/g_local.h
+++ include/g_local.h
@@ -75,7 +75,7 @@
 
 #define MOD_NAME				("KTX")
 #define MOD_FULLNAME			("KTX: Kombat Teams eXtreme")
-#define MOD_VERSION				("1.41")
+#define MOD_VERSION				("1.42-dev")
 #define MOD_BUILD_DATE			(__DATE__ ", " __TIME__)
 #define MOD_SERVERINFO_MOD_KEY	("ktxver")
 #define MOD_URL					("https://github.com/QW-Group/ktx")
