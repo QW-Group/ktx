4c9a30bc43a0a6c373f5dff826e2e93fef354868
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2023-05-15 00:49:00 -0700

    CLIENT: update stuffcmd for SendSpecInfo

--- src/client.c
+++ src/client.c
@@ -4457,8 +4457,7 @@ void CheckTeamStatus()
 void SendSpecInfo()
 {
 	gedict_t *t, *p;
-	int cl;
-	char *tracking, *nick;
+	int cl, tr;
 
 	static double lastupdate = 0;
 
@@ -4471,23 +4470,8 @@ void SendSpecInfo()
 
 	for (t = world; (t = find_spc(t));)
 	{
-		if (t->ct != ctSpec)
-		{
-			continue;
-		}
-
 		cl = NUM_FOR_EDICT(t) - 1;
-		tracking = TrackWhom(t);
-
-		if (strnull(nick = ezinfokey(t, "k_nick"))) // get nick, if any, do not send name, client can guess it too
-		{
-			nick = ezinfokey(t, "k");
-		}
-
-		if (nick[0] && nick[1] && nick[2] && nick[3])
-		{
-			nick[4] = 0; // truncate nick to 4 symbols
-		}
+		tr = NUM_FOR_EDICT(PROG_TO_EDICT(t->s.v.goalentity)) - 1;	// num for player spec is tracking
 
 		for (p = world; (p = find_client(p));)
 		{
@@ -4496,7 +4480,7 @@ void SendSpecInfo()
 				continue; // ignore self
 			}
 
-			stuffcmd_flags(p, STUFFCMD_IGNOREINDEMO, "//specinfo %d \"%s\" \"%s\"\n", cl, nick, tracking);
+			stuffcmd_flags(p, STUFFCMD_IGNOREINDEMO, "//spi %d %d\n", cl, tr);
 		}
 	}
 }
